96b80f49331676a52079083ab6a4db99a0c2f7cc
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-01 14:56:02 +1000

    🎉 Update

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,9 @@
+source .env
+
+# Setup the Fantom directory
+echo $EMAIL >> nodes/fantom/.api
+echo $PASSWORD >> nodes/fantom/.api
+
+echo $PASSWORD >> nodes/fantom/.password
+
 docker-compose up -d
