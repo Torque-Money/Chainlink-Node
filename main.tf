253ab43b0d31602e1853586b7137353307736a08
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-20 22:56:03 +1000

    🎉 Update

--- main.tf
+++ main.tf
@@ -7,16 +7,71 @@ terraform {
   }
 }
 
-# Set the variable value in *.tfvars file
-# or using -var="do_token=..." CLI option
-variable "do_token" {}
+variable "do_token" {
+    type = string
+}
 
-# Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
 }
 
-# Create a web server
+# Create the droplet
 resource "digitalocean_droplet" "web" {
-  # ...
+    image = "ubuntu-20-04-x64"
+    name = "Chainlink"
+    region = "sfo3"
+    size = "s-2vcpu-2gb"
+}
+
+# Create the firewall
+resource "digitalocean_firewall" "web" {
+    name = "only-22-80-and-443"
+
+    droplet_ids = [digitalocean_droplet.web.id]
+
+    inbound_rule {
+        protocol         = "tcp"
+        port_range       = "22"
+        source_addresses = ["192.168.1.0/24", "2002:1:2::/48"]
+    }
+
+    inbound_rule {
+        protocol         = "tcp"
+        port_range       = "80"
+        source_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    inbound_rule {
+        protocol         = "tcp"
+        port_range       = "443"
+        source_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    inbound_rule {
+        protocol         = "icmp"
+        source_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    outbound_rule {
+        protocol              = "tcp"
+        port_range            = "53"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    outbound_rule {
+        protocol              = "udp"
+        port_range            = "53"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    outbound_rule {
+        protocol              = "icmp"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+}
+
+# Setup domain name
+resource "digitalocean_domain" "default" {
+  name       = "link.torque.money"
+  ip_address = digitalocean_droplet.foo.ipv4_address
 }
