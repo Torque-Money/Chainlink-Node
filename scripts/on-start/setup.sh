c3b859ae76ad78044833c8c274312d8ae8218fb9
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-21 13:04:55 +1000

    🎉 Update

--- scripts/on-start/setup.sh
+++ scripts/on-start/setup.sh
@@ -3,7 +3,6 @@
 echo "Starting setup.sh..."
 
 echo "*       *       *       *       *       psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f /usr/scripts/sql/whitelist.sql" >> /etc/crontabs/root
-# echo "*       *       *       *       *       bash -c 'psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f /usr/scripts/sql/whitelist.sql'" >> /etc/crontabs/root
 
 crontab -l
 crond
