b2aa0c0549bfc5270b6b35a028ceb8e5c089ba85
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-21 12:57:29 +1000

    🎉 Update

--- scripts/on-start/setup.sh
+++ scripts/on-start/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 echo "Starting setup.sh..."
-# echo "*       *       *       *       *       run-parts /etc/periodic/1min" >> /etc/crontabs/root
-echo "*       *       *       *       *       touch /usr/scripts/sh/WORKED.txt" >> /etc/crontabs/root
+# echo "*       *       *       *       *       touch /usr/scripts/sh/WORKED.txt" >> /etc/crontabs/root
 
 crontab -l
 crond
