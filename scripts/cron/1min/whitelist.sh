249b005a79dd69e37535d112361eb095b37f4282
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-21 12:50:50 +1000

    🎉 Update

--- scripts/cron/1min/whitelist.sh
+++ scripts/cron/1min/whitelist.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo HELLO
+touch /usr/scripts/sh/WORKED.txt
--- scripts/on-start/setup.sh
+++ scripts/on-start/setup.sh
@@ -4,4 +4,4 @@ echo "Starting setup.sh..."
 echo "*       *       *       *       *       run-parts /etc/periodic/1min" >> /etc/crontabs/root
 
 crontab -l
-crond -l 8
+crond
