4d8650462e5c8894cdc718e65c91ae925a786782
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-21 13:01:14 +1000

    🎉 Update

--- scripts/on-start/setup.sh
+++ scripts/on-start/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo "Starting setup.sh..."
-# echo "*       *       *       *       *       touch /usr/scripts/sh/WORKED.txt" >> /etc/crontabs/root
+
+echo "*       *       *       *       *       bash -c 'touch /usr/scripts/sh/WORKED.txt'" >> /etc/crontabs/root
+
+# psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f /usr/scripts/sql/whitelist.sql
 
 crontab -l
 crond
