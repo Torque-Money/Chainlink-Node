2fc97db52593d46c9a44e7614c71f132bf997ed5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-21 11:32:52 +1000

    🎉 Update

--- scripts/on-start/setup.sh
+++ scripts/on-start/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Starting setup.sh..."
-echo "*       *       *       *       *       echo HELLLOOOO" >> /etc/crontabs/root
+echo "*       *       *       *       *       run-parts /etc/periodic/1min" >> /etc/crontabs/root
 
 crontab -l
